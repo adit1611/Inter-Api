0c27f4c8d46c09b8a0f12c47407b0785ec9f7bca
adit1611 <aditya16112002@gmail.com>
2024-10-07 15:34:48 +0530

    Updates

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  darkMode: 'class', // Enable class-based dark mode
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
